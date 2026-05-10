e63c20dd3eb3fc1654783fd24e7aaf9ae981b656
Michael Orr <michael@orr.co>
2026-05-10 19:54:53 -0400

    fix filter sorting

--- components/filters.go
+++ components/filters.go
@@ -16,15 +16,15 @@ func Filters(icons map[search.FileIcon]int, width int) string {
 	b.WriteByte('\n')
 	types := slices.Collect(maps.Keys(icons))
 	slices.SortFunc(types, func(a, b search.FileIcon) int {
-		return strings.Compare(a.Icon, b.Icon)
+		return icons[b] - icons[a]
 	})
 
 	for _, icon := range types {
 		iconStyle := style.DefaultStyle.Foreground(lipgloss.Color(icon.Color))
 
 		line := iconStyle.Render(icon.Icon) + style.DefaultStyle.Render(fmt.Sprintf(" %d", icons[icon]))
-		line = line + style.DefaultStyle.Render(strings.Repeat(" ", max(0, width-lipgloss.Width(line))), "\n")
-		b.WriteString(line)
+		line = line + style.DefaultStyle.Render(strings.Repeat(" ", max(0, width-lipgloss.Width(line))))
+		b.WriteString(line + "\n")
 	}
 	return b.String()
 }
